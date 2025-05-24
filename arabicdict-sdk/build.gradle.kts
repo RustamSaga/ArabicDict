40f5d67a6fccdc0fc3185cfcfbedf180926004e4
RustamSaga <rustam.saga94@gmail.com>
2025-05-23 19:30:13 -0700

    Add entities and doc categories and Auth settings. Not finished yet.

--- arabicdict-sdk/build.gradle.kts
+++ arabicdict-sdk/build.gradle.kts
@@ -1,71 +1,36 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.multiplatform)
-    alias(libs.plugins.jetbrains.compose.multiplatform)
-    alias(libs.plugins.jetbrains.compose.compiler)
+    alias(libs.plugins.arabicdict.kmplib)
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.ktorfit)
 }
 
 kotlin {
-    jvmToolchain(11)
-}
-
-kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
-    jvm("desktop")
-
     sourceSets {
-        val desktopMain by getting
-
-        androidMain.dependencies {
-        }
         commonMain.dependencies {
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.contentNegotiation)
             implementation(libs.kotlinx.serialization.json)
-            implementation(libs.kotlinx.datetime)
+
+            implementation(libs.ktor.client.core)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.contentNegotiation)
             implementation(libs.ktor.client.auth)
             implementation(libs.ktorfit)
-            api(libs.ktorfit.converters.response)
         }
+
+        androidMain.dependencies {
+            implementation(libs.ktor.client.engine.okhttp)
+        }
+
         desktopMain.dependencies {
+            implementation(libs.ktor.client.engine.okhttp)
         }
-        iosMain.dependencies {}
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.engine.darwin)
+        }
     }
 }
 
 android {
     namespace = "dev.arabicdictionary.pro.arabicdictsdk"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
 }
